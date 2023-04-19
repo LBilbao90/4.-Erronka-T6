9506b9b454342731a75afc881b54eb7cf9eebe84
“Andoni” <andoni.fernandezlo@elorrieta-errekamari.com>
2023-04-19 12:59:37 +0200

    Parte indibiduala (erabiltzailea eta maiala)

--- ERRONKA_4_T6/src/Objetuak/Erablitzaile.java
+++ ERRONKA_4_T6/src/Objetuak/Erablitzaile.java
@@ -1,5 +1,38 @@
 package Objetuak;
 
-public class Erablitzaile {
-
+public class Erablitzaile extends Pertsona{
+	private String erabiltzaile_izena;
+	private double diru_kopuru_historikoa;
+	private double diru_kopuru_momentukoa;
+	
+	public Erablitzaile(String erabiltzaile_izena, double diru_kopuru_historikoa, double diru_kopuru_momentukoa) {
+		super();
+		this.erabiltzaile_izena = erabiltzaile_izena;
+		this.diru_kopuru_historikoa = diru_kopuru_historikoa;
+		this.diru_kopuru_momentukoa = diru_kopuru_momentukoa;
+	}
+	
+	public String getErabiltzaile_izena() {
+		return erabiltzaile_izena;
+	}
+	public void setErabiltzaile_izena(String erabiltzaile_izena) {
+		this.erabiltzaile_izena = erabiltzaile_izena;
+	}
+	public double getDiru_kopuru_historikoa() {
+		return diru_kopuru_historikoa;
+	}
+	public void setDiru_kopuru_historikoa(double diru_kopuru_historikoa) {
+		this.diru_kopuru_historikoa = diru_kopuru_historikoa;
+	}
+	public double getDiru_kopuru_momentukoa() {
+		return diru_kopuru_momentukoa;
+	}
+	public void setDiru_kopuru_momentukoa(double diru_kopuru_momentukoa) {
+		this.diru_kopuru_momentukoa = diru_kopuru_momentukoa;
+	}
+	public void erakutsi() {
+		System.out.println("Datuak: "/*super. (pertsonen datuak hemen erakusten ditugu ere)*/
+			+erabiltzaile_izena+", diru kopuru historikoa:"+diru_kopuru_historikoa
+				+", diru kopuru momentukoa:"+diru_kopuru_momentukoa);
+	}
 }
--- ERRONKA_4_T6/src/Objetuak/Maila.java
+++ ERRONKA_4_T6/src/Objetuak/Maila.java
@@ -1,5 +1,50 @@
 package Objetuak;
 
 public class Maila {
-
+	private String maila_izena;
+	private double biderkatzailea;
+	private int kopurua;
+	private String NAN;
+	private int id_maila;
+	
+	
+	public Maila(String maila_izena, double biderkatzailea, int kopurua, String nan, int id_maila) {
+		this.maila_izena = maila_izena;
+		this.biderkatzailea = biderkatzailea;
+		this.kopurua = kopurua;
+		this.NAN = nan;
+		this.id_maila = id_maila;
+	}
+	
+	public String getMaila_izena() {
+		return maila_izena;
+	}
+	public void setMaila_izena(String maila_izena) {
+		this.maila_izena = maila_izena;
+	}
+	public double getBiderkatzailea() {
+		return biderkatzailea;
+	}
+	public void setBiderkatzailea(double biderkatzailea) {
+		this.biderkatzailea = biderkatzailea;
+	}
+	public int getKopurua() {
+		return kopurua;
+	}
+	public void setKopurua(int kopurua) {
+		this.kopurua = kopurua;
+	}
+	public String getNAN() {
+		return NAN;
+	}
+	public void setNAN(String nan) {
+		NAN = nan;
+	}
+	public int getId_maila() {
+		return id_maila;
+	}
+	public void setId_maila(int id_maila) {
+		this.id_maila = id_maila;
+	}
 }
+	
