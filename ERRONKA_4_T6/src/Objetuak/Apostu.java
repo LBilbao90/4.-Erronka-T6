fa8b41eb3dc462e3a42689a4706a2b8a211e4cc9
“Iker” <iker.gorostiagala@elorrieta-errekamari.com>
2023-04-20 13:18:23 +0200

    apostua, baimenak test eginda

--- ERRONKA_4_T6/src/Objetuak/Apostu.java
+++ ERRONKA_4_T6/src/Objetuak/Apostu.java
@@ -1,5 +1,94 @@
 package Objetuak;
 
+import java.util.Objects;
+
 public class Apostu {
+	
+	private int id_apustua;
+	private int id_joko;
+	private String NAN;
+	private int apostu_kantitatea;
+	private int apostu_emaitza;
+	
+	//KONSTRUKTOREA
+	public Apostu(int id_apustua, int id_joko, String nAN, int apostu_kantitatea, int apostu_emaitza) {
+		super();
+		this.id_apustua = id_apustua;
+		this.id_joko = id_joko;
+		NAN = nAN;
+		this.apostu_kantitatea = apostu_kantitatea;
+		this.apostu_emaitza = apostu_emaitza;
+	}
+	
+	//GETTERS & SETTERS
+	public int getId_apustua() {
+		return id_apustua;
+	}
+	
+	public void setId_apustua(int id_apustua) {
+		this.id_apustua = id_apustua;
+	}
+	public int getId_joko() {
+		return id_joko;
+	}
+	public void setId_joko(int id_joko) {
+		this.id_joko = id_joko;
+	}
+	public String getNAN() {
+		return NAN;
+	}
+	public void setNAN(String nAN) {
+		NAN = nAN;
+	}
+
+	public int getApostu_kantitatea() {
+		return apostu_kantitatea;
+	}
+	public void setApostu_kantitatea(int apostu_kantitatea) {
+		this.apostu_kantitatea = apostu_kantitatea;
+	}
+	public int getApostu_emaitza() {
+		return apostu_emaitza;
+	}
+	public void setApostu_emaitza(int apostu_emaitza) {
+		this.apostu_emaitza = apostu_emaitza;
+	}
+	
+	
+	//METODOAK
+	
+	public void apustua_egin() {
+		
+	}
+
+	//TOESTRING
+	
+	@Override
+	public String toString() {
+		return "Apustuak [id_apustua=" + id_apustua + ", id_joko=" + id_joko + ", NAN=" + NAN + ", apostu_kantitatea="
+				+ apostu_kantitatea + ", apostu_emaitza=" + apostu_emaitza + "]";
+	}
+
+	//HASHCODE
+	@Override
+	public int hashCode() {
+		return Objects.hash(NAN, apostu_emaitza, apostu_kantitatea, id_apustua, id_joko);
+	}
+
+	//EQUALS
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Apostu other = (Apostu) obj;
+		return Objects.equals(NAN, other.NAN) && apostu_emaitza == other.apostu_emaitza
+				&& apostu_kantitatea == other.apostu_kantitatea && id_apustua == other.id_apustua
+				&& id_joko == other.id_joko;
+	}
+	
 
 }
--- ERRONKA_4_T6/src/Objetuak/Baimena.java
+++ ERRONKA_4_T6/src/Objetuak/Baimena.java
@@ -1,5 +1,127 @@
 package Objetuak;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class Baimena {
+	
+	private String erabiltzailea;
+	private int baimen_mota;
+	private Date hasiera_data;
+	private Date bukaera_data;
+	
+	private Pertsona pertsona;
+	
+	//CONSTRUCTOR
+	public Baimena(String erabiltzailea, int baimen_mota, Date hasiera_data, Date bukaera_data, Pertsona pertsona) {
+		super();
+		this.erabiltzailea = erabiltzailea;
+		this.baimen_mota = baimen_mota;
+		this.hasiera_data = hasiera_data;
+		this.bukaera_data = bukaera_data;
+		this.pertsona = pertsona;
+	}
+
+
+	//GETTERS & SETTERS
+	public String getErabiltzailea() {
+		return erabiltzailea;
+	}
+	public void setErabiltzailea(String erabiltzailea) {
+		this.erabiltzailea = erabiltzailea;
+	}
+
+
+	public int getBaimen_mota() {
+		return baimen_mota;
+	}
+	public void setBaimen_mota(int baimen_mota) {
+		this.baimen_mota = baimen_mota;
+	}
+
+
+	public Date getHasiera_data() {
+		return hasiera_data;
+	}
+	public void setHasiera_data(Date hasiera_data) {
+		this.hasiera_data = hasiera_data;
+	}
+
+
+	public Date getBukaera_data() {
+		return bukaera_data;
+	}
+	public void setBukaera_data(Date bukaera_data) {
+		this.bukaera_data = bukaera_data;
+	}
+
+
+	public Pertsona getPertsona() {
+		return pertsona;
+	}
+	public void setPertsona(Pertsona pertsona) {
+		this.pertsona = pertsona;
+	}
+
+	
+	//METODOAK
+	public int baimena_artu(int nahai_duen_baimena) {
+		
+		if( this.baimen_mota == nahai_duen_baimena) {
+			return baimen_mota;
+		}else{
+			return -1;
+		}
+		
+		
+	}
+	
+	
+	public void baimena_kanporatu(int baimena_kanp, String erabiltzailea) {
+		
+		if(this.baimen_mota == baimena_kanp && this.erabiltzailea.equalsIgnoreCase(erabiltzailea)) {
+			
+		}else {}
+	
+	}
+	
+	
+	public boolean balidatu_erabiltzailea_baimenarako() {
+		
+		return false;
+	}
+	
+
+	//TOESTRING
+	@Override
+	public String toString() {
+		return "Baimena [erabiltzailea=" + erabiltzailea + ", baimen_mota=" + baimen_mota + ", hasiera_data="
+				+ hasiera_data + ", bukaera_data=" + bukaera_data + ", pertsona=" + pertsona + "]";
+	}
+
+	
+	
+	//HASCHODE
+	@Override
+	public int hashCode() {
+		return Objects.hash(baimen_mota, bukaera_data, erabiltzailea, hasiera_data, pertsona);
+	}
+
+
+	//EQUALS
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Baimena other = (Baimena) obj;
+		return baimen_mota == other.baimen_mota && Objects.equals(bukaera_data, other.bukaera_data)
+				&& Objects.equals(erabiltzailea, other.erabiltzailea)
+				&& Objects.equals(hasiera_data, other.hasiera_data) && Objects.equals(pertsona, other.pertsona);
+	}
+	
 
 }
